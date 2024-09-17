cc76e409b4f0b7ae42d94de09a427c0477c0b3a2
Surendra Patidar <131394762+SurendraPatidar1@users.noreply.github.com>
2024-09-17 06:29:03 +0000

    script.js

--- script.js
+++ script.js
@@ -33,8 +33,9 @@ let recognition = new speechRecognition();
 recognition.onresult = (event) => {
     let currentIndex = event.resultIndex; 
     let transcript = event.results[currentIndex][0].transcript;
+    console.log(transcript);
     content.innerText = transcript;
-    takeCommand(transcript.toLowerCase());
+    takeCommand(transcript.toLowerCase().trim());
 };
 
 btn.addEventListener("click", () => {
@@ -45,36 +46,62 @@ btn.addEventListener("click", () => {
 });
 
 function takeCommand(message) {
-    btn.style.display="flex";
-    voice.style.display="none";
+    btn.style.display = "flex";
+    voice.style.display = "none";
 
     if (message.includes("hello") || message.includes("hey")) {
         speak("Hello Sir, what can I help you with?");
     } 
-    else if (message.includes("Who are you")){
+    else if (message.includes("who are you") || message.includes("hu r u")) { // Make sure to use lowercase
         speak("I am a virtual assistant, created by Mister Surendra Sir.");
-    }else if(message.includes("open youtube")){
-        speak("OPening youtube ....");
-        window.open("https://www.youtube.com/","_blank");
-    }
-    else if(message.includes("open Github")){
-        speak("OPening Github ....");
-        window.open("https://github.com/SurendraPatidar1","_blank");
-    }
-    else if(message.includes("open instagram")){
-        speak("OPening Instagram ....");
-        window.open("https://www.instagram.com/","_blank");
-    }
-    else if(message.includes("open GFG")){
-        speak("OPening GeeksForGeeks ....");
-        window.open("https://www.geeksforgeeks.org/","_blank");
-    }
-    else if(message.includes("open facebook")){
-        speak("OPening Facebook ....");
-        window.open("https://www.facebook.com/","_blank");
-    }
-    else{
-        speak(`This is what i found on internet regarding ${message}`);
+    }    
+    else if (message.includes("open youtube")) {
+        speak("Opening YouTube ....");
+        window.open("https://www.youtube.com/", "_blank");
+    } 
+    else if (message.includes("open Github")) {
+        speak("Opening GitHub ....");
+        window.open("https://github.com/SurendraPatidar1", "_blank");
+    } 
+    else if (message.includes("open instagram")) {
+        speak("Opening Instagram ....");
+        window.open("https://www.instagram.com/", "_blank");
+    } 
+    else if (message.includes("open gfg") || message.includes("open geeks for geeks")) {
+        speak("Opening GeeksForGeeks ....");
+        window.open("https://www.geeksforgeeks.org/", "_blank");
+    }     
+    else if (message.includes("open facebook")) {
+        speak("Opening Facebook ....");
+        window.open("https://www.facebook.com/", "_blank");
+    } 
+    else if (message.includes("open Calculator")) {
+        speak("Opening Calculator ....");
+        window.open("calculator://", "_blank");
+    } 
+    else if (message.includes("open Whatsapp")) {
+        speak("Opening WhatsApp ....");
+        window.open("whatsapp://", "_blank");
+    } 
+    else if (message.includes("open word")) {
+        speak("Opening Word ....");
+        window.open("word://", "_blank");
+    } 
+    else if (message.includes("open spotify")) {
+        speak("Opening Spotify ....");
+        window.open("spotify://", "_blank");
+    } 
+    else if (message.includes("time")) {
+        let time = new Date().toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric" });
+        speak(time);
+    } 
+    else if (message.includes("date")) {
+        let date = new Date().toLocaleDateString(undefined, { day: "numeric", month: "short" });
+        speak(date);
+    } 
+    else {
+        let finalText = "This is what I found on the internet regarding " + message;
+        speak(finalText);
         window.open(`https://search.yahoo.com/search?fr=mcafee&type=E211US1289G0&p=${message}`);
     }
 }
